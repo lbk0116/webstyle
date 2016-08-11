72e43792e3ea91a31055401ae8e036307bd58dcb
wanghaipeng28 <wanghaipeng28@163.com>
2016-08-11 19:35:22 +0800

    web style and script v0.4

--- static/src/js/customScript.js
+++ static/src/js/customScript.js
@@ -77,7 +77,7 @@ $(document).ready(function(){
      function addSearchKey(){
          var $div=$('<div class="col-md-12">'+
                  '<ul data-tag="work_age"><li>工作年限：</li><li><a href="1">1年</a></li><li><a href="2">2年</a></li><li><a href="3">3年</a></li><li><a href="4">4年</a></li><li><a href="5">5年</a></li><li><a href="6">6年</a></li><li><a href="7">7年以上</a></li></ul>'+
-                 '<ul data-tag="certificate_ids"><li>证书：</li><li><a href="cisco">Cisco</a></li><li><a href="华为">华为</a></li><li><a href="华三">华三</a></li><li><a href="F5">F5</a></li><li><a href="IBM">IBM</a></li></ul>'+
+                 '<ul data-tag="certificate_institutions_id"><li>证书：</li><li><a href="cisco">Cisco</a></li><li><a href="华为">华为</a></li><li><a href="华三">华三</a></li><li><a href="F5">F5</a></li><li><a href="IBM">IBM</a></li></ul>'+
                  '<ul data-tag="level"><li>级别：</li><li><a href="1">1级</a></li><li><a href="2">2级</a></li><li><a href="3">3级</a></li><li><a href="4">4级</a></li><li><a href="5">5级</a></li><li><a href="6">6级</a></li></ul>'+
                  '<ul data-tag="category"><li>人员归属：</li><li><a href="在公司">在公司</a></li><li><a href="在合同中">在合同中</a></li><li><a href="赠送">赠送</a></li><li><a href="开发">开发</a></li><li><a href="其他">其他</a></li></ul>'+
                  '<ul data-tag="project_id"><li>客户：</li><li><a href="中行">中行</a></li><li><a href="建行">建行</a></li><li><a href="农行">农行</a></li><li><a href="国开">国开</a></li><li><a href="广大">广大</a></li><li><a href="农发">农发</a></li><li><a href="信达">信达</a></li></ul>'+
@@ -119,16 +119,16 @@ $(document).ready(function(){
                      $("form button.oe_apply:first").trigger("submit");
                      var divNum=$(".oe_searchview_facets div").length;
                      indexObj.level=divNum-2;
-                 }else if(tag==="certificate_ids"){
-                    if(indexObj.certificate_ids){
-                        removeSelect(indexObj.certificate_ids);
+                 }else if(tag==="certificate_institutions_id"){
+                    if(indexObj.certificate_institutions_id){
+                        removeSelect(indexObj.certificate_institutions_id);
                     }
                      $("form .searchview_extended_prop_op").val("ilike");
                      var value=$(this).attr("href");
                      $("form .searchview_extended_prop_value>input.field_char").val(value);
                      $("form button.oe_apply:first").trigger("submit");
                      var divNum=$(".oe_searchview_facets div").length;
-                     indexObj.certificate_ids=divNum-2;
+                     indexObj.certificate_institutions_id=divNum-2;
                  }else if(tag==="category"){
                     if(indexObj.category){
                         removeSelect(indexObj.category);
@@ -171,38 +171,38 @@ $(document).ready(function(){
      }
  });
 
-//固定表头脚本
-$(document).ready(function () {
-    setTimeout(function () {
-        fixedHeader();
-        console.log($("a.oe_vm_switch_list"))
-        $("a.oe_vm_switch_list").click(function () {
-            fixedHeader();
-            console.log(123)
-        });
-    },5000)
-    function fixedHeader() {
-             var n=0;//记录复选框的点击次数
-            var $div = $("<div class='fixedHead'></div>");
-            var $table=$("<table></table>");
-            $table.append($("table.oe_list_content>thead").clone(true));
-            $div.append($table);
-            $("div.oe_view_manager_wrapper>div").append($div);
-            $.each($("tr.oe_list_header_columns:first>th"),function (i,v) {
-                var width=parseInt($(v).css("width"));
-                console.log(width);
-                var cloneDate=$("tr.oe_list_header_columns:last>th")[i];
-                $(cloneDate).attr("width",width-12);
-            });
-            $(".fixedHead input.oe_list_record_selector").click(function(){
-                if(n===0){
-                    n=1;
-                    for(var i=0;i<2;i++){
-                        $("div.oe_list input.oe_list_record_selector").trigger("click");
-                    }
-                }else{
-                    $("div.oe_list input.oe_list_record_selector").trigger("click");
-                }
-            });
-    }
-});
+// //固定表头脚本
+// $(document).ready(function () {
+//     setTimeout(function () {
+//         fixedHeader();
+//         console.log($("a.oe_vm_switch_list"))
+//         $("a.oe_vm_switch_list").click(function () {
+//             fixedHeader();
+//             console.log(123)
+//         });
+//     },5000)
+//     function fixedHeader() {
+//              var n=0;//记录复选框的点击次数
+//             var $div = $("<div class='fixedHead'></div>");
+//             var $table=$("<table></table>");
+//             $table.append($("table.oe_list_content>thead").clone(true));
+//             $div.append($table);
+//             $("div.oe_view_manager_wrapper>div").append($div);
+//             $.each($("tr.oe_list_header_columns:first>th"),function (i,v) {
+//                 var width=parseInt($(v).css("width"));
+//                 console.log(width);
+//                 var cloneDate=$("tr.oe_list_header_columns:last>th")[i];
+//                 $(cloneDate).attr("width",width-12);
+//             });
+//             $(".fixedHead input.oe_list_record_selector").click(function(){
+//                 if(n===0){
+//                     n=1;
+//                     for(var i=0;i<2;i++){
+//                         $("div.oe_list input.oe_list_record_selector").trigger("click");
+//                     }
+//                 }else{
+//                     $("div.oe_list input.oe_list_record_selector").trigger("click");
+//                 }
+//             });
+//     }
+// });
