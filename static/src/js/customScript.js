8d71615230bb2c6d3eedc035a8c4695fc38ba8dc
wanghaipeng28 <wanghaipeng28@163.com>
2016-08-13 21:45:04 +0800

    add function of duoxuan

--- static/src/js/customScript.js
+++ static/src/js/customScript.js
@@ -61,7 +61,6 @@ $(document).ready(function(){
  $(document).ready(function(){
      function boot(){
          var stra=$("li.active>a.oe_menu_toggler>span.oe_menu_text").html();
-         console.log(111);
          if(stra){
              stra=stra.trim();
              if(stra==="人力资源"){
@@ -91,21 +90,50 @@ $(document).ready(function(){
          var indexObj={};
          //{"work_age":["1","2","3","4","5","6","7"],"certificate_institutions_id":["cisco","华为","华三","F5","IBM"],"level":["1","2","3","4","5","6"],"category":["在公司","在合同中","赠送","开发","其他"],"project_id":["中行","建行","农行","国开","广大","农发","信达"]}
          function refreshData(obj,t) {
-             //一开始先去掉所有查询
-             var closes=$("div.oe_searchview_facets span.oe_facet_remove");
+             //开始先删除相关的已选择的选项标签
+             var tag={level:"Level",
+                 certificate_institutions_id:"证书颁发机构或行业",
+                 category:"人员所属",
+                 project_id:"Project",
+                 work_age:"Work age"};
+             $("span.oe_facet_values>span.oe_facet_value").each(function (i,span) {
+                 var html=$(span).html().trim();
+                 if(html.indexOf(tag[t])===0){
+                     $(this).parent("span").siblings("span.oe_facet_remove").trigger("click");
+                 }
+             });
 
              //开始根据页面上勾选的选择去自动添加查询
-
-             for(var i=0;i<obj[t].length;i++){
-                 $("form .searchview_extended_prop_field").val(t);
-                 $("form .searchview_extended_prop_field").trigger("change");
+             var length=obj[t].length;
+             for(var i=1;i<length;i++){
+                 $('.oe_add_condition').trigger("click");
+             }
+             for(var i=0;i<length;i++){
+                 $("form li:eq("+i+") .searchview_extended_prop_field").val(t);
+                 $("form li:eq("+i+") .searchview_extended_prop_field").trigger("change");
                  if(t==="level"){
-                     $("form .searchview_extended_prop_op").val("=");
-                     $("form .searchview_extended_prop_value>select").val(value);
-
+                     $("form li:eq("+i+") .searchview_extended_prop_op").val("=");
+                     $("form li:eq("+i+") .searchview_extended_prop_value>select").val(obj[t][i]);
+                 }else if(t==="certificate_institutions_id"){
+                     $("form li:eq("+i+") .searchview_extended_prop_op").val("ilike");
+                     $("form li:eq("+i+") .searchview_extended_prop_value>input.field_char").val(obj[t][i]);
+                 }else if(t==="category"){
+                     $("form li:eq("+i+") .searchview_extended_prop_op").val("=");
+                     $("form li:eq("+i+") .searchview_extended_prop_value>select").val(obj[t][i]);
+                 }else if(t==="project_id"){
+                     $("form li:eq("+i+") .searchview_extended_prop_op").val("ilike");
+                     $("form li:eq("+i+") .searchview_extended_prop_value>input.field_char").val(obj[t][i]);
+                 }else if(t==="work_age"){
+                     if(obj[t][i]=="7"){
+                         $("form li:eq("+i+") .searchview_extended_prop_op").val(">=");
+                     }else{
+                         $("form li:eq("+i+") .searchview_extended_prop_op").val("=");
+                     }
+                     $("form li:eq("+i+") .searchview_extended_prop_value>input.field_integer").val(obj[t][i]);
                  }
+             }
+             if(length>0){
                  $("form button.oe_apply:first").trigger("submit");
-
              }
          }
          $("div.col-md-12>ul>li>a").click(function(e){
@@ -128,7 +156,7 @@ $(document).ready(function(){
                  }
                  indexObj[tag].push(value);
              }
-             console.log(JSON.stringify(indexObj));
+             // console.log(JSON.stringify(indexObj));
             refreshData(indexObj,tag);
 
          });
