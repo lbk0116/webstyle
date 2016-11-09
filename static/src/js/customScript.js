070ad0822e579d79904b56bf03176954a3bab1a7
wanghaipeng28 <wanghaipeng28@163.com>
2016-11-09 16:07:05 +0800

    更新工作组字段名称

--- static/src/js/customScript.js
+++ static/src/js/customScript.js
@@ -152,7 +152,7 @@ $(document).ready(function () {
          var $div=$('<div class="col-md-12">'+
                  '<ul data-tag="certificate_institutions_id"><li><span class="oe_i">w</span>证书</li><li><a href="cisco">Cisco</a></li><li><a href="华为">华为</a></li><li><a href="华三">华三</a></li><li><a href="F5">F5</a></li><li><a href="IBM">IBM</a></li></ul>'+
                  '<ul data-tag="level"><li><span class="oe_i">w</span>级别</li><li><a href="1">1级</a></li><li><a href="2">2级</a></li><li><a href="3">3级</a></li><li><a href="4">4级</a></li><li><a href="5">5级</a></li><li><a href="6">6级</a></li></ul>'+
-                 '<ul data-tag="project_id"><li><span class="oe_i">w</span>工作组</li><li><a href="中行">中行</a></li><li><a href="建行">建行</a></li><li><a href="农行">农行</a></li><li><a href="国开">国开</a></li><li><a href="光大">光大</a></li><li><a href="农发">农发</a></li><li><a href="信达">信达</a></li></ul>'+
+                 '<ul data-tag="working_team_id"><li><span class="oe_i">w</span>工作组</li><li><a href="中行">中行</a></li><li><a href="建行">建行</a></li><li><a href="农行">农行</a></li><li><a href="国开">国开</a></li><li><a href="光大">光大</a></li><li><a href="农发">农发</a></li><li><a href="信达">信达</a></li></ul>'+
                  '<ul data-tag="work_age"><li><span class="oe_i">w</span>工作年限</li><li><a href="1">1年</a></li><li><a href="2">2年</a></li><li><a href="3">3年</a></li><li><a href="4">4年</a></li><li><a href="5">5年</a></li><li><a href="6">6年</a></li><li><a href="7">7年以上</a></li></ul>'+
                  '<ul data-tag="category"><li><span class="oe_i">w</span>人员状态</li><li><a href="公司储备">公司储备</a></li><li><a href="合同在岗">合同在岗</a></li><li><a href="合同备岗">合同备岗</a></li><li><a href="合同赠送">合同赠送</a></li><li><a href="公司项目">公司项目</a></li></ul>'+
              '</div>');
@@ -160,13 +160,13 @@ $(document).ready(function () {
          $(".oe_searchview_drawer").append($div);
          var isTrigger=false;
          var indexObj={};
-         //{"work_age":["1","2","3","4","5","6","7"],"certificate_institutions_id":["cisco","华为","华三","F5","IBM"],"level":["1","2","3","4","5","6"],"category":["在公司","在合同中","赠送","开发","其他"],"project_id":["中行","建行","农行","国开","广大","农发","信达"]}
+         //{"work_age":["1","2","3","4","5","6","7"],"certificate_institutions_id":["cisco","华为","华三","F5","IBM"],"level":["1","2","3","4","5","6"],"category":["在公司","在合同中","赠送","开发","其他"],"working_team_id":["中行","建行","农行","国开","广大","农发","信达"]}
          function refreshData(obj,t) {
              //开始先删除相关的已选择的选项标签
              var tag={level:"Level",
                  certificate_institutions_id:"certificate_institutions",
                  category:"人员状态",
-                 project_id:"Project",
+                 working_team_id:"Working team",
                  work_age:"Work age"};
              $("span.oe_facet_values>span.oe_facet_value").each(function (i,span) {
                  var html=$(span).html().trim();
@@ -194,7 +194,7 @@ $(document).ready(function () {
                  }else if(t==="category"){
                      $("form li:eq("+i+") .searchview_extended_prop_op").val("=");
                      $("form li:eq("+i+") .searchview_extended_prop_value>select").val(obj[t][i]);
-                 }else if(t==="project_id"){
+                 }else if(t==="working_team_id"){
                      $("form li:eq("+i+") .searchview_extended_prop_op").val("ilike");
                      $("form li:eq("+i+") .searchview_extended_prop_value>input.field_char").val(obj[t][i]);
                  }else if(t==="work_age"){
